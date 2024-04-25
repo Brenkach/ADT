3368ed18da74d79fd21ac9aa5151b32ed24c81f6
Denus <Brenkachd@gmail.com>
2024-04-25 21:28:19 +0300

    Add all methods for Stack and StackArray, for Example: push, pop, isEmpty, peek, size.

--- ADT/Stack.h
+++ ADT/Stack.h
@@ -10,4 +10,37 @@ private:
 
 public:
     Stack() : list() {}
+
+    bool IsEmpty() const {
+        return list.isEmpty();
+    }
+
+    T Peek()const {
+        if (list.isEmpty()) {
+            throw out_of_range("Stack is empty");
+        }
+        return list[Size() - 1];
+    }
+
+    void Push(const T& value) {
+        list.pushBack(value);
+    }
+
+    T Pop() {
+        if (IsEmpty()) {
+            throw underflow_error("Stack is empty");
+        }
+        T value = list[Size() - 1];
+        list.popBack();
+        return value;
+    }
+
+    size_t Size()const {
+        return list.Size();
+    }
+
+    friend ostream& operator<<(ostream& os, const Stack<T>& stack) {
+        os << stack.list;
+        return os;
+    }
 };
--- ADT/StackArray.h
+++ ADT/StackArray.h
@@ -8,6 +8,16 @@ private:
     T* elements;
     size_t capacity;
     size_t top;
+
+    void Resize(size_t newCapacity) {
+        T* newItems = new T[newCapacity];
+        for (size_t i = 0; i < top; ++i) {
+            newItems[i] = elements[i];
+        }
+        delete[] elements;
+        elements = newItems;
+        capacity = newCapacity;
+    }       // Зміна розміра масиву
 public:
 
     StackArray(size_t initialCapacity = 10) :capacity(initialCapacity), top(0) {
@@ -16,5 +26,46 @@ public:
     ~StackArray() {
         delete[] elements;
     }
+    bool IsEmpty() const {
+        return top == 0;
+    }
+
+    T Peek() const {
+        if (IsEmpty()) {
+            throw out_of_range("Stack is empty");
+        }
+        return elements[top - 1];
+    }
+
+
+    void Push(const T& value) {
+        if (top == capacity) {
+            Resize(capacity * 2); // Подвоюємо розмір масиву, якщо стек повний
+        }
+        elements[top++] = value;
+    }
+
+    T Pop() {
+        if (IsEmpty()) {
+            throw underflow_error("Stack is empty");
+        }
+        T value = elements[--top];
+        if (top > 0 && top == capacity / 4) {
+            Resize(capacity / 2); // Зменшуємо розмір масиву, якщо стек наполовину порожній
+        }
+        return value;
+    }
+
+    size_t Size() const {
+        return top;
+    }
+
+    friend ostream& operator<<(ostream& os, const StackArray<T>& stack) {
+        for (size_t i = 0; i < stack.top; ++i) {
+            os << stack.elements[i] << " ";
+        }
+        return os;
+    }
+
 
 };
