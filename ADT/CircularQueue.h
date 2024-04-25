6e1cea564c65bea08dda93f5bd8db30c4cbfcfdb
Denus <Brenkachd@gmail.com>
2024-04-25 21:10:47 +0300

    Add all methods for Circular Queue and Circular QueueArray, for Example: Enqueue, Dequeue, Peek, IsEmpty, IsFull.

--- ADT/CircularQueue.h
+++ ADT/CircularQueue.h
@@ -12,4 +12,39 @@ public:
     CircularQueue() = default;
 
     CircularQueue(size_t capacity) : list(), capacity(capacity) {}
+
+    bool isEmpty()const {
+        return list.Size() == 0;
+    }
+
+    bool isFull()const {
+        return list.Size() == capacity;
+    }
+
+    void enqueue(const T& value) {
+        list.pushBack(value);
+    }
+
+    T dequeue() {
+        if (isEmpty()) {
+            throw underflow_error("Queue is empty |'_'|");
+        }
+        T value = list[0];
+        list.popFront();
+        return value;
+    }
+
+    T peek() const {
+        if (isEmpty()) {
+            throw out_of_range("Queue is empty |'_'|");
+        }
+        return list[0];
+    }
+
+
+
+    friend ostream& operator<<(ostream& os, const CircularQueue<T>& circularQueue) {
+        os << circularQueue.list;
+        return os;
+    }
 };
